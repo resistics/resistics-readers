a1b6b0990d92cdd6c9d6fa48059c380b8223bf6e
nss350 <nss350@gmail.com>
2021-06-19 16:54:34 +0100

    Adding more description to the init

--- resistics_readers/__init__.py
+++ resistics_readers/__init__.py
@@ -2,7 +2,20 @@
 A package with readers for various formats of electromagnetic geophysical time
 series data with an initial focus on magnetotellurics.
 
-This package is an extension to resistics and requires the resistics package
+This package is an extension to resistics and requires the resistics package.
+
+Resistics readers broadly puts time series data into two catergories that it
+treats differently:
+
+- Single sampling frequency data (e.g. ATS, SPAM, Lemi)
+- Multi sampling frequency data (e.g. Phoenix, Miniseed)
+
+For single sampling frequency files, the approach is to use the data files as
+they are in the resistics projects and import the appropriate time data readers.
+
+Unfortunately, resistics does not currently support multi-sampling frequency
+data files. Therefore, the approach here is to reformat data from the various
+sampling frequencies into the resistics Numpy time data format.
 """
 from importlib.metadata import version, PackageNotFoundError
 
