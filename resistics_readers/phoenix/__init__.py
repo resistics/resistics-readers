f75f1f8af3595ce5e6f927f9498294613e50e54d
nss350 <nss350@gmail.com>
2021-06-21 00:02:10 +0100

    Adding read_metadata to the imports. In the future, might need to name this read_mtu5_metadata and reformat_mtu5 to avoid conflicts for other types of Phoenix data

--- resistics_readers/phoenix/__init__.py
+++ resistics_readers/phoenix/__init__.py
@@ -3,4 +3,4 @@ Readers for Phoenix data, including:
 
 - Readers for time series data from MTU5(A)
 """
-from resistics_readers.phoenix.mtu5 import TimeReaderTS, reformat  # noqa
+from resistics_readers.phoenix.mtu5 import TimeReaderTS, reformat, read_metadata  # noqa
